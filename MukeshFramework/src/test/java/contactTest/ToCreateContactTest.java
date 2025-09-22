23af8e57d6596f05f843f3cd6bb80620da0e76aa
Mukesh Praveen S <Mukesh Praveen S@MUKESH>
2025-09-22 15:05:19 +0530

    Mukesh Profile

---
+++ MukeshFramework/src/test/java/contactTest/ToCreateContactTest.java
@@ -0,0 +1,9 @@
+package contactTest;
+
+public class ToCreateContactTest {
+
+	public static void main(String[] args) {
+		System.out.println("Mukesh! What a marvelous name");
+	}
+
+}
---
+++ MukeshFramework/src/test/java/orgTest/ToCreateOrgTest.java
@@ -0,0 +1,7 @@
+package orgTest;
+
+public class ToCreateOrgTest {
+public static void main(String[] args) {
+	System.out.println("Avengers Formed Successfully");
+}
+}
